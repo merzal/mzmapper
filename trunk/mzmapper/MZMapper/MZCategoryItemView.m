269fdde8a358643f4bae454d483304de196f427d
merzal@gmail.com <merzal@gmail.com@797abc4e-4524-4845-87d3-a8d83a9084e1>
2012-10-19 05:42:36 +0000

    - added solution for drag & drop category items - collect point objects into an array by the parsing process - draw bus stations to the map

--- trunk/mzmapper/MZMapper/MZCategoryItemView.m
+++ trunk/mzmapper/MZMapper/MZCategoryItemView.m
@@ -13,8 +13,7 @@
 {
     UILongPressGestureRecognizer* _longPressGesture;
     UIPanGestureRecognizer* _panGesture;
-    CGFloat _firstX;
-    CGFloat _firstY;
+    CGPoint _delta;
 }
 @end
 
@@ -30,11 +29,6 @@
         
         _longPressGesture = [[UILongPressGestureRecognizer alloc] initWithTarget:self action:@selector(enableMove:)];
         [self addGestureRecognizer:_longPressGesture];
-        
-        
-        _panGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(moveImage:)];
-        [_panGesture setEnabled:NO];
-        [self addGestureRecognizer:_panGesture];
     }
     return self;
 }
@@ -43,19 +37,10 @@
 {
     NSLog(@"%s",__PRETTY_FUNCTION__);
     
+    CGPoint recognizedAtPoint = [gestureRecognizer locationInView:self.window.rootViewController.view];
+    
     if (gestureRecognizer.state == UIGestureRecognizerStateBegan)
     {
-        NSLog(@"began");
-        UIView *view = [gestureRecognizer view];
-        
-        for (UIGestureRecognizer *gr in view.gestureRecognizers)
-        {
-            if ([gr isKindOfClass:[UIPanGestureRecognizer class]])
-            {
-                [gr setEnabled:YES];
-            }
-        }
-        
         _movedImageView = [[UIImageView alloc] initWithImage:self.itemImage];
         [_movedImageView setFrame:CGRectMake(3.0, 3.0, 44.0, 44.0)];
         [_movedImageView setAlpha:0.7];
@@ -63,33 +48,17 @@
         CGPoint convertedCenterPoint = [self convertPoint:_movedImageView.center toView:self.window.rootViewController.view];
         [_movedImageView setCenter:convertedCenterPoint];
         [self.window.rootViewController.view addSubview:_movedImageView];
-        NSLog(@"type: %@",[self.window.rootViewController class]);
+        
+        _delta = CGPointMake(recognizedAtPoint.x - _movedImageView.center.x, recognizedAtPoint.y - _movedImageView.center.y);
     }
-}
-
-// shift the piece's center by the pan amount
-// reset the gesture recognizer's translation to {0, 0} after applying so the next callback is a delta from the current position
-- (void)moveImage:(UIPanGestureRecognizer *)gestureRecognizer
-{
-    NSLog(@"%s",__PRETTY_FUNCTION__);
-    CGPoint translatedPoint = [gestureRecognizer translationInView:gestureRecognizer.view];
-    
-    if(gestureRecognizer.state == UIGestureRecognizerStateBegan)
+    else if (gestureRecognizer.state == UIGestureRecognizerStateChanged)
     {
-        _firstX = _movedImageView.center.x;
-        _firstY = _movedImageView.center.y;
+        [_movedImageView setCenter:CGPointMake(recognizedAtPoint.x - _delta.x, recognizedAtPoint.y - _delta.y)];
     }
-    
-    translatedPoint = CGPointMake(_firstX+translatedPoint.x, _firstY+translatedPoint.y);
-    
-    [_movedImageView setCenter:translatedPoint];
-    
-    if (gestureRecognizer.state == UIGestureRecognizerStateEnded)
+    else if (gestureRecognizer.state == UIGestureRecognizerStateEnded)
     {
         [_movedImageView setAlpha:1.0];
         [_movedImageView release], _movedImageView = nil;
-        
-        [_panGesture setEnabled:NO];
     }
 }
 
@@ -97,7 +66,6 @@
 // An empty implementation adversely affects performance during animation.
 - (void)drawRect:(CGRect)rect
 {
-    
     // Drawing code
     
     //draw item image
